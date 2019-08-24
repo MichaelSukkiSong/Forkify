73541417b9ad7bdf25eb105fedc719a06bd950cb
sa77irak <sa77irak@gmail.com>
2019-08-24 18:27:01 +0900

    building the search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,5 @@
-import axios from 'axios'; // axios is a popular HTTP request library. install it from npm as --save. 
-                           //axios is better than fetch because it works on older browsers/automatically returns json(so dont need 2 steps as fetch)/and also better in error handling.
+import Search from './models/Search';
 
-async function getResults(query) {
-    const key = '4060c2889a4fd3a70ca5c876c19abcf6';
-    try {
-        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        alert(error);
-    }
-}
-getResults('pizza');
-
-
-// https://www.food2fork.com/api/search
-// 4060c2889a4fd3a70ca5c876c19abcf6
-// https://www.food2fork.com/api/search?key=4060c2889a4fd3a70ca5c876c19abcf6&q=pizza
-
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
---
+++ src/js/models/Search.js
@@ -0,0 +1,24 @@
+import axios from 'axios'; // axios is a popular HTTP request library. install it from npm as --save. 
+                           //axios is better than fetch because it works on older browsers/automatically returns json(so dont need 2 steps as fetch)/and also better in error handling.
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const key = '4060c2889a4fd3a70ca5c876c19abcf6';
+        try {
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
+
+// https://www.food2fork.com/api/search
+// 4060c2889a4fd3a70ca5c876c19abcf6
+// https://www.food2fork.com/api/search?key=4060c2889a4fd3a70ca5c876c19abcf6&q=pizza
+
