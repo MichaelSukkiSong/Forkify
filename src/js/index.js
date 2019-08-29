23c7f8c87f92bdaceb23bac0ee162b4817bcbcbb
sa77irak <sa77irak@gmail.com>
2019-08-29 16:24:01 +0900

    wrapping up: final considerations

--- src/js/index.js
+++ src/js/index.js
@@ -16,7 +16,6 @@ import { elements, renderLoader, clearLoader } from './views/base';
  * - Liked recipes
 */
 const state = {};
-window.state = state;
 
 /**
  * SEARCH CONTROLLER
@@ -72,7 +71,6 @@ elements.searchResPages.addEventListener('click', e => {
 const controlRecipe = async () => {
     // Get ID from url
     const id = window.location.hash.replace('#', '');
-    console.log(id);
 
     if (id) {
         // Prepare UI for changes
@@ -114,6 +112,7 @@ const controlRecipe = async () => {
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+
 /**
  * LIST CONTROLLER
  */
@@ -152,8 +151,6 @@ elements.shopping.addEventListener('click', e => {
 /**
  * LIKE CONTROLLER
  */
-
-
 const controlLike = () => {
     if (!state.likes) state.likes = new Likes();
     const currentID = state.recipe.id;
@@ -226,8 +223,6 @@ elements.recipe.addEventListener('click', e => {
     }
 });
 
-
-window.l = new List();
 
 
 
