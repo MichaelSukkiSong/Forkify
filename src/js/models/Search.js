f5787f0718275ce6442f1dd46f3f8ec44a746514
sa77irak <sa77irak@gmail.com>
2020-05-06 13:43:18 +0900

    changes to the api

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,21 +1,23 @@
-import axios from 'axios'; // axios is a popular HTTP request library. install it from npm as --save.
-                           //axios is better than fetch because it works on older browsers/automatically returns json(so dont need 2 steps as fetch)/and also better in error handling.
-import { key } from '../config';
+import axios from "axios"; // axios is a popular HTTP request library. install it from npm as --save.
+//axios is better than fetch because it works on older browsers/automatically returns json(so dont need 2 steps as fetch)/and also better in error handling.
+import { key } from "../config";
 
 export default class Search {
-    constructor(query) {
-        this.query = query;
-    }
+  constructor(query) {
+    this.query = query;
+  }
 
-    async getResults() { // async functions return a promise.
-        try {
-            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
-            this.result = res.data.recipes;
-            //console.log(this.result);
-        } catch (error) {
-            alert(error);
-        }
+  async getResults() {
+    // async functions return a promise.
+    try {
+      // const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+      const res = await axios(
+        `https://forkify-api.herokuapp.com/api/search?&q=${this.query}`
+      );
+      this.result = res.data.recipes;
+      //console.log(this.result);
+    } catch (error) {
+      alert(error);
     }
+  }
 }
-
-
